5170079e76936e7991e5d7e9ab463ef07cca51e0
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-08-18 12:28:42 -0300

    Spring initialize

---
+++ src/main/java/com/manuel/zaguan_inmobiliarias/ZaguanInmobiliariasApplication.java
@@ -0,0 +1,13 @@
+package com.manuel.zaguan_inmobiliarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaguanInmobiliariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaguanInmobiliariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuel/zaguan_inmobiliarias/ZaguanInmobiliariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.zaguan_inmobiliarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaguanInmobiliariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
