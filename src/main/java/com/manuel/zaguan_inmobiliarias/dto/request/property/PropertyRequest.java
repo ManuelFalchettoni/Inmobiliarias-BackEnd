5bd839e0590b03ac7fa4aad3537184e550f50cbe
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-08-19 16:39:08 -0300

    Property request response and mapper

--- src/main/java/com/manuel/zaguan_inmobiliarias/dto/request/property/PropertyRequest.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/dto/request/property/PropertyRequest.java
@@ -1,4 +1,51 @@
 package com.manuel.zaguan_inmobiliarias.dto.request.property;
 
+import com.manuel.zaguan_inmobiliarias.enums.property.PropertySituation;
+import com.manuel.zaguan_inmobiliarias.enums.property.PropertyStatus;
+import com.manuel.zaguan_inmobiliarias.enums.property.PropertyType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class PropertyRequest {
+
+    @NotBlank
+    private String address;
+
+    @NotNull
+    private PropertyType type;
+
+    @NotBlank
+    private String location;
+
+    @NotNull
+    private Long idAgency;
+
+    @PastOrPresent
+    private LocalDate year;
+
+    @PositiveOrZero
+    private int rooms;
+
+    @Positive
+    private int size;
+
+    @NotNull
+    private PropertySituation situation;
+
+    @NotNull
+    private PropertyStatus status;
+
+    @PositiveOrZero
+    private int floorNumber;
 }
--- src/main/java/com/manuel/zaguan_inmobiliarias/dto/response/property/PropertyResponse.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/dto/response/property/PropertyResponse.java
@@ -1,4 +1,49 @@
 package com.manuel.zaguan_inmobiliarias.dto.response.property;
 
+import com.manuel.zaguan_inmobiliarias.enums.property.PropertySituation;
+import com.manuel.zaguan_inmobiliarias.enums.property.PropertyStatus;
+import com.manuel.zaguan_inmobiliarias.enums.property.PropertyType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class PropertyResponse {
+
+    private Long id;
+
+    private String address;
+
+    private Boolean active;
+
+    private PropertyType type;
+
+    private String location;
+
+    private Long idAgency;
+
+    private LocalDate year;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private int rooms;
+
+    private int size;
+
+    private PropertySituation situation;
+
+    private PropertyStatus status;
+
+    private int floorNumber;
+
+    private List<String> photoUrls = new ArrayList<>();
 }
