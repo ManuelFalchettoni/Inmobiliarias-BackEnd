dcd3ca7c844c68153996b1a2ac517a51701e0159
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-08-19 16:44:51 -0300

    Property POST controller

--- src/main/java/com/manuel/zaguan_inmobiliarias/controller/property/PropertyPostController.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/controller/property/PropertyPostController.java
@@ -1,4 +1,27 @@
 package com.manuel.zaguan_inmobiliarias.controller.property;
 
+import com.manuel.zaguan_inmobiliarias.dto.request.property.PropertyRequest;
+import com.manuel.zaguan_inmobiliarias.dto.response.property.PropertyResponse;
+import com.manuel.zaguan_inmobiliarias.service.property.PropertyCreatorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/properties")
+@RequiredArgsConstructor
 public class PropertyPostController {
+
+    private final PropertyCreatorService propertyCreatorService;
+
+    @PostMapping
+    public ResponseEntity<PropertyResponse> create(@Valid @RequestBody PropertyRequest request) {
+        PropertyResponse response = propertyCreatorService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
