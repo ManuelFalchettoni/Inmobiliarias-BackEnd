9f898ec42d1a43e92bb4970e6130b810c448703c
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-08-27 20:15:58 -0300

    fix: Agency exists on property creation

--- src/main/java/com/manuel/zaguan_inmobiliarias/repository/property/JpaPropertyRepository.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/repository/property/JpaPropertyRepository.java
@@ -1,6 +1,7 @@
 package com.manuel.zaguan_inmobiliarias.repository.property;
 
 import com.manuel.zaguan_inmobiliarias.entity.property.Property;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -15,6 +16,7 @@ public interface JpaPropertyRepository  extends JpaRepository<Property, Long> {
     @EntityGraph(attributePaths = "photos") //problema N+1 preguntar
     Optional<Property> findByIdAndActiveTrue(Long id);
 
+    @BatchSize(size = 20)
     Page<Property> findAllByActiveTrue(Pageable pageable);
 
     Page<Property> findAllByIdAgencyAndActiveTrue(Long idAgency, Pageable pageable);
--- src/main/java/com/manuel/zaguan_inmobiliarias/service/property/PropertyCreatorService.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/service/property/PropertyCreatorService.java
@@ -3,7 +3,9 @@ package com.manuel.zaguan_inmobiliarias.service.property;
 import com.manuel.zaguan_inmobiliarias.dto.request.property.PropertyRequest;
 import com.manuel.zaguan_inmobiliarias.dto.response.property.PropertyResponse;
 import com.manuel.zaguan_inmobiliarias.entity.property.Property;
+import com.manuel.zaguan_inmobiliarias.exception.agency.AgencyNotFoundException;
 import com.manuel.zaguan_inmobiliarias.mapper.property.PropertyMapper;
+import com.manuel.zaguan_inmobiliarias.repository.agency.JpaAgencyRepository;
 import com.manuel.zaguan_inmobiliarias.repository.property.JpaPropertyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,16 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PropertyCreatorService {
     private final JpaPropertyRepository jpaPropertyRepository;
+    private final JpaAgencyRepository jpaAgencyRepository;
     private final PropertyMapper propertyMapper;
 
-    public PropertyCreatorService (JpaPropertyRepository jpaPropertyRepository, PropertyMapper propertyMapper){
+    public PropertyCreatorService (JpaPropertyRepository jpaPropertyRepository, PropertyMapper propertyMapper, JpaAgencyRepository jpaAgencyRepository){
         this.jpaPropertyRepository = jpaPropertyRepository;
+        this.jpaAgencyRepository = jpaAgencyRepository;
         this.propertyMapper = propertyMapper;
     }
 
     @Transactional
     public PropertyResponse create(PropertyRequest request){
-        Property property = jpaPropertyRepository.save(propertyMapper.toEntity(request));
-        return propertyMapper.toResponse(property);
+        if (jpaAgencyRepository.existsAgencyById(request.getIdAgency())){
+            Property property = jpaPropertyRepository.save(propertyMapper.toEntity(request));
+            return propertyMapper.toResponse(property);
+        }
+        throw new AgencyNotFoundException(request.getIdAgency());
+
     }
 }
