3787bcfd19759d46ddc7b1836fe22c2302ef4d87
Guido <guidoperroud@gmail.com>
2026-08-27 20:32:18 -0300

    feat: auth register

---
+++ src/main/java/com/manuel/zaguan_inmobiliarias/service/auth/AuthAgencyRegisterService.java
@@ -0,0 +1,30 @@
+package com.manuel.zaguan_inmobiliarias.service.auth;
+
+import com.manuel.zaguan_inmobiliarias.dto.request.auth.agency.AuthAgencyRequest;
+import com.manuel.zaguan_inmobiliarias.dto.response.agency.AgencyResponse;
+import com.manuel.zaguan_inmobiliarias.entity.agency.Agency;
+import com.manuel.zaguan_inmobiliarias.exception.auth.DuplicateResourceException;
+import com.manuel.zaguan_inmobiliarias.mapper.agency.AgencyMapper;
+import com.manuel.zaguan_inmobiliarias.repository.agency.JpaAgencyRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthAgencyRegisterService {
+    private final JpaAgencyRepository jpaAgencyRepository;
+    private final AgencyMapper agencyMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public AgencyResponse agencyRegister (AuthAgencyRequest agencyRequest) {
+        if (jpaAgencyRepository.existsAgencyByEmail(agencyRequest.getEmail())){
+            throw new DuplicateResourceException("User with email: " + agencyRequest.getEmail() + " already exists.");
+        }
+
+        Agency agency = agencyMapper.toEntity(agencyRequest);
+        agency.setPassword(passwordEncoder.encode(agencyRequest.getPassword()));
+
+        return agencyMapper.toResponse(jpaAgencyRepository.save(agency));
+    }
+}
---
+++ src/main/java/com/manuel/zaguan_inmobiliarias/service/auth/AuthUserRegisterService.java
@@ -0,0 +1,30 @@
+package com.manuel.zaguan_inmobiliarias.service.auth;
+
+import com.manuel.zaguan_inmobiliarias.dto.request.auth.user.AuthUserRequest;
+import com.manuel.zaguan_inmobiliarias.dto.response.user.UserResponse;
+import com.manuel.zaguan_inmobiliarias.entity.user.User;
+import com.manuel.zaguan_inmobiliarias.exception.auth.DuplicateResourceException;
+import com.manuel.zaguan_inmobiliarias.mapper.user.UserMapper;
+import com.manuel.zaguan_inmobiliarias.repository.user.JpaUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthUserRegisterService {
+    private final JpaUserRepository jpaUserRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+public UserResponse userRegister (AuthUserRequest userRequest){
+    if (jpaUserRepository.existsByEmail(userRequest.getEmail())){
+        throw new DuplicateResourceException("User with email: " + userRequest.getEmail() + " already exists.");
+    }
+
+    User user = userMapper.toEntity(userRequest);
+    user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+
+    return userMapper.toResponse(jpaUserRepository.save(user));
+}
+}
