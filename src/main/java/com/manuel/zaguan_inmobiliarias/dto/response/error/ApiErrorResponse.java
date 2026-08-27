c1cecfe7fd1ceeaf4a34abc2cdc61a9712ed44e4
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-08-27 15:47:18 -0300

    feat: GlobalExceptionHandler

---
+++ src/main/java/com/manuel/zaguan_inmobiliarias/dto/response/error/ApiErrorResponse.java
@@ -0,0 +1,23 @@
+package com.manuel.zaguan_inmobiliarias.dto.response.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class ApiErrorResponse {
+
+    private LocalDateTime timestamp;
+
+    private int status;
+
+    private String error;
+
+    private String message;
+
+    private String path;
+}
