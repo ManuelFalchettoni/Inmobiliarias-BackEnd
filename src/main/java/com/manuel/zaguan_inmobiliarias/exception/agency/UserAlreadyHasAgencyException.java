204e15544e0803db8c4de6b5278adfc7e26f10c7
guido perroud <guidoperroud@gmail.com>
2026-08-30 01:16:17 -0300

    feat: agency exception

---
+++ src/main/java/com/manuel/zaguan_inmobiliarias/exception/agency/UserAlreadyHasAgencyException.java
@@ -0,0 +1,7 @@
+package com.manuel.zaguan_inmobiliarias.exception.agency;
+
+public class UserAlreadyHasAgencyException extends RuntimeException {
+    public UserAlreadyHasAgencyException(String message) {
+        super(message);
+    }
+}
