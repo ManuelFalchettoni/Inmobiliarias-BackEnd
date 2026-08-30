ce340171611aec664cab1572129ece8c40a57a74
guido perroud <guidoperroud@gmail.com>
2026-08-30 01:13:15 -0300

    Refactor: user repository

--- src/main/java/com/manuel/zaguan_inmobiliarias/repository/user/JpaUserRepository.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/repository/user/JpaUserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,5 +20,7 @@ public interface JpaUserRepository extends JpaRepository<User, Long>, JpaSpecifi
     boolean existsById(@NonNull Long id);
 
     boolean existsByEmail(@NonNull String email);
+
+    List<User> findByAgency_Id(@NonNull Long agency_Id);
 
 }
