62787d81279bd5ea2f54d618f19817659938313b
guido perroud <guidoperroud@gmail.com>
2026-09-01 17:49:38 -0300

    refactor: auth user dto

--- src/main/java/com/manuel/zaguan_inmobiliarias/dto/request/auth/user/AuthUserRequest.java
+++ src/main/java/com/manuel/zaguan_inmobiliarias/dto/request/auth/user/AuthUserRequest.java
@@ -1,9 +1,7 @@
 package com.manuel.zaguan_inmobiliarias.dto.request.auth.user;
 
-import com.manuel.zaguan_inmobiliarias.enums.user.UserRol;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,6 +28,4 @@ public class AuthUserRequest {
     @Size(min = 8, max = 15, message = "phoneNumber must be between 8 and 15 characters.")
     private int phoneNumber;
 
-    @NotNull
-    private UserRol rol;
 }
